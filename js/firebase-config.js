808c3d5410d602c9b1ac3f2be241f51954dd50e0
juwalliser-creator <juwalliser@proton.me>
2026-07-23 17:33:15 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,13 @@
 // Trage hier deine Firebase-Daten ein (Anleitung: FIREBASE-SETUP.md)
 window.FIREBASE_CONFIG = {
-  apiKey: "HIER_DEIN_API_KEY",
-  authDomain: "HIER_DEIN_PROJECT.firebaseapp.com",
-  projectId: "HIER_DEIN_PROJECT_ID",
-  storageBucket: "HIER_DEIN_PROJECT.appspot.com",
-  messagingSenderId: "HIER_DEINE_SENDER_ID",
-  appId: "HIER_DEINE_APP_ID",
+  apiKey: "AIzaSyC6ImpOpHjlysnpFsLWv2W3uGaZ63A4fzk",
+  authDomain: "einkaufsliste-53cf8.firebaseapp.com",
+  projectId: "einkaufsliste-53cf8",
+  storageBucket: "einkaufsliste-53cf8.firebasestorage.app",
+  messagingSenderId: "311289967845",
+  appId: "1:311289967845:web:36255aeb238c14ad8b900c",
 };
 
 // Gemeinsamer Gruppencode für dich und deinen Bruder (gleicher Code = gleiche Daten)
 // Du kannst das auch später in der App ändern oder per Link ?gruppe=DEINCODE teilen
-window.DEFAULT_GROUP_CODE = "FAMILIE";
+window.DEFAULT_GROUP_CODE = "family";
